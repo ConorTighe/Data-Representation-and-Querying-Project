f05ef77fd312168d64c61bcf2deaab876bc5807c
ConorTighe1995 <conor.tighe22@gmail.com>
2016-11-21 21:33:22 +0000

    Added menu, nav bar

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This repository contains a single web application called Meal Manager that I will be submitting as my project.
 
 ## Project Overview:
-The aim of my project is to creat a hub where a user can find a recipe, view the ingrediants and save the recipe locally.
+The aim of my project is to create a hub where a user can find a recipe, view the ingrediants and save the recipe locally.
 
 The API I will be using is:
 ![alt text](https://s3.amazonaws.com/mashape-production-logos/apis/53aa5b2be4b0f2c975470ff1_medium "food")
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,28 @@
-
-// ajax for interating with API,Database wtc.
 $(document).ready(function(){
-     
+      var i=1;
+     $("#add_row").click(function(){
+      $('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='name"+i+"' type='text' placeholder='Name' class='form-control input-md'  /> </td><td><input  name='mail"+i+"' type='text' placeholder='Mail'  class='form-control input-md'></td><td><input  name='mobile"+i+"' type='text' placeholder='Mobile'  class='form-control input-md'></td>");
+
+      $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+      i++; 
+  });
+     $("#delete_row").click(function(){
+    	 if(i>1){
+		 $("#addr"+(i-1)).html('');
+		 i--;
+		 }
+	 });
+
 });
 
+$.ajax({
+    url: 'https://community-food2fork.p.mashape.com/get?key=0ee5a01caf7f7c3512b54978628f1a4e&rId=37859', // The URL to the API. You can get this in the API page of the API you intend to consume
+    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+    data: {}, // Additional parameters here
+    dataType: 'json',
+    success: function(data) { console.dir((data.source)); },
+    error: function(err) { alert(err); },
+    beforeSend: function(xhr) {
+    xhr.setRequestHeader("X-Mashape-Authorization", "VVSftRLP2lmshYkf353HVyk4G8ajp12QUm2jsn8IflyCbYxtDg"); // Enter here your Mashape key
+    }
+});

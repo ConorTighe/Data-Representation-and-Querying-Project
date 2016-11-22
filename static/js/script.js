714c002ae9bfacedd09d2a2244e764ff8fcae423
ConorTighe1995 <conor.tighe22@gmail.com>
2016-11-22 18:51:51 +0000

    Integrated API, added table with dynamic data and other small changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,31 @@
-$(document).ready(function(){
-      var i=1;
-     $("#add_row").click(function(){
-      $('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='name"+i+"' type='text' placeholder='Name' class='form-control input-md'  /> </td><td><input  name='mail"+i+"' type='text' placeholder='Mail'  class='form-control input-md'></td><td><input  name='mobile"+i+"' type='text' placeholder='Mobile'  class='form-control input-md'></td>");
+var result = [];
 
-      $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
-      i++; 
-  });
-     $("#delete_row").click(function(){
-    	 if(i>1){
-		 $("#addr"+(i-1)).html('');
-		 i--;
-		 }
-	 });
-
-});
 
 $.ajax({
     url: 'https://community-food2fork.p.mashape.com/get?key=0ee5a01caf7f7c3512b54978628f1a4e&rId=37859', // The URL to the API. You can get this in the API page of the API you intend to consume
     type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
     data: {}, // Additional parameters here
     dataType: 'json',
-    success: function(data) { console.dir((data.source)); },
+    success: function(data) { result = JSON.parse(JSON.stringify(data.recipe));
+                            console.dir(result);
+                            var cnt = 0;
+                            $('#location').append(
+                                $.map(result.ingredients, function (ignore, index) {
+                                    cnt = cnt +1;
+                                return '<tr><td>' + cnt +'</td><td>' + result.ingredients[index] + '</td></tr>';
+                                    
+                            }).join()
+);},
     error: function(err) { alert(err); },
     beforeSend: function(xhr) {
     xhr.setRequestHeader("X-Mashape-Authorization", "VVSftRLP2lmshYkf353HVyk4G8ajp12QUm2jsn8IflyCbYxtDg"); // Enter here your Mashape key
     }
+});
+
+ 
+$(document).ready(function(){
+
+   
+      
+
 });
